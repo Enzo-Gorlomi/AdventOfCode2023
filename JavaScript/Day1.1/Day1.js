0c6f3a132600f444ca965270495d5bb297367cd1
Enzo-Gorlomi <55324727+Enzo-Gorlomi@users.noreply.github.com>
2023-12-19 23:40:52 +0100

    finished Day 1 Task 1

---
+++ JavaScript/Day1.1/Day1.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+function getSum(string){
+    let digitsOnly = string.replace(/\D/g, '');
+    
+    if(digitsOnly.length == 0){return 0;}
+
+    let len = digitsOnly.length;
+    let first = digitsOnly.charAt(0);
+    let last = digitsOnly.charAt(len - 1);
+    let num = first + last
+    
+    return parseInt(num)
+}
+
+fs.readFile('JavaScript\\Day1.1\\input.txt', (err, data) => {
+    if (err) throw err;
+    let text = data.toString();
+    let lines = text.split(/\n/);
+
+    let calibrationSum = 0;
+    let numberOfRows = lines.length;
+    for(let i = 0; i < numberOfRows; i++){
+        calibrationSum += getSum(lines[i]);
+    }
+
+    console.log("Answer:", calibrationSum);
+})
+
+// Answer found - 56108
---
+++ JavaScript/Day1.1/tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+lines;
+// let text;
